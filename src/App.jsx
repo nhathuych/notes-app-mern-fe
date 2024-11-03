bc1b1b120ff54d2e90cedc6e1d7fa2cf06d31bb1
nhathuych <nhathuych@gmail.com>
2024-11-03 10:18:44 +0700

    install react-router-dom & setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import SignUp from './pages/SignUp/SignUp'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/dashboard' element={<Home/>} />
+    </Routes>
+  </Router>
+)
 
 function App() {
   return (
     <div>
-      <h1 className='text-purple-900'>Notes app mern stack | FE</h1>
+      {routes}
     </div>
   )
 }
